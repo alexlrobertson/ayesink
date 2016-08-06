c749c58dd583dbf089b01a5b396db342129fa2d3
Alex Robertson <alexleonardrobertson@gmail.com>
2016-08-06 16:31:35 -0500

    fix lint

--- test.js
+++ test.js
@@ -1,98 +1,81 @@
-var assert = require('assert');
-var async = require('./');
+/* global describe, it */
+const assert = require('assert');
+const async = require('./');
 
-describe('async', function() {
-  describe('has a sequence method that', function() {
-    it('runs functions in sequence', function(done) {
-      var fun1 = function(cb) {
-        setTimeout(cb.bind(null, null, 'test'), 10);
-      };
-      var fun2 = function(cb, data) {
-        setTimeout(cb.bind(null, null, data + 'ing'), 10);
-      };
+describe('async', () => {
+  describe('has a sequence method that', () => {
+    it('runs functions in sequence', done => {
+      const fun1 = cb => setTimeout(cb.bind(null, null, 'test'), 10);
+      const fun2 = (cb, data) => setTimeout(cb.bind(null, null, `${data}ing`), 10);
 
       // returns a thunk
-      async.sequence([fun1, fun2])(function(err, data) {
+      async.sequence([fun1, fun2])((err, data) => {
         assert.equal(data, 'testing');
         done();
       });
     });
 
-    it('correctly handles sync functions in sequence', function(done) {
-      var fun1 = function(cb) {
-        cb(null, 'test1');
-      };
-      var fun2 = function(cb, data) {
-          cb(null, data);
-      };
+    it('correctly handles sync functions in sequence', done => {
+      const fun1 = cb => cb(null, 'test1');
+      const fun2 = (cb, data) => cb(null, data);
 
       // returns a thunk
-      async.sequence([fun1, fun2])(function(err, data) {
+      async.sequence([fun1, fun2])((err, data) => {
         assert.equal(data, 'test1');
         done();
       });
     });
 
-    it('handles delayed thunk invocation', function(done) {
-      var fun1 = function(cb) {
-        cb(null, 'test2');
-      };
-      var fun2 = function(cb, data) {
-        cb(null, data.toUpperCase());
-      };
+    it('handles delayed thunk invocation', done => {
+      const fun1 = cb => cb(null, 'test2');
+      const fun2 = (cb, data) => cb(null, data.toUpperCase());
 
       // returns a thunk
-      var setter = async.sequence([fun1, fun2]);
+      const setter = async.sequence([fun1, fun2]);
 
-      setTimeout(function() {
-        setter(function(err, data) {
+      setTimeout(() =>
+        setter((err, data) => {
           assert.equal(data, 'TEST2');
           done();
-        });
-      }, 100)
+        })
+      , 100);
     });
   });
 
-  describe('has a parallel method that', function() {
-    it('runs functions in parallel', function(done) {
-      var fun1 = function(cb) {
+  describe('has a parallel method that', () =>
+    it('runs functions in parallel', done => {
+      function fun1(cb) {
         setTimeout(cb.bind(null, null, 'test'), 10);
-      };
-      var fun2 = function(cb) {
+      }
+      function fun2(cb) {
         setTimeout(cb.bind(null, null, 'ing'), 10);
-      };
+      }
 
       // returns a thunk
-      async.parallel([fun1, fun2])(function(err, data) {
+      async.parallel([fun1, fun2])((err, data) => {
         assert.deepEqual(data, ['test', 'ing']);
         done();
       });
-    });
-  });
+    })
+  );
 
-  describe('has a race method that', function() {
-    it('uses the first completing function', function(done) {
-      var fun1 = function(ms) {
-        return function(cb) {
-          setTimeout(cb.bind(null, null, 'test'), ms);
-        };
-      };
-      var fun2 = function(ms) {
-        return function(cb) {
-          setTimeout(cb.bind(null, null, 'ing'), ms);
-        };
-      };
+  describe('has a race method that', () =>
+    it('uses the first completing function', done => {
+      function fun1(ms) {
+        return cb => setTimeout(cb.bind(null, null, 'test'), ms);
+      }
+      function fun2(ms) {
+        return cb => setTimeout(cb.bind(null, null, 'ing'), ms);
+      }
 
       // returns a thunk
-      async.race([fun1(10), fun2(20)])(function(err, data) {
+      async.race([fun1(10), fun2(20)])((err, data) => {
         assert.equal(data, 'test');
-        async.race([fun1(20), fun2(10)])(function(err, data) {
+        async.race([fun1(20), fun2(10)])((err, data) => {
           assert.equal(data, 'ing');
           done();
         });
-
       });
-    });
-  });
-
+    })
+  );
 });
